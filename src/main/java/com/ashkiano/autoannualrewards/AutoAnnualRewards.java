7c7b3d9c90cdac32fb0bcc0cf3f21ab5495a47cf
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-04 23:23:30 +0100

    init

---
+++ src/main/java/com/ashkiano/autoannualrewards/AutoAnnualRewards.java
@@ -0,0 +1,38 @@
+package com.ashkiano.autoannualrewards;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Calendar;
+
+public class AutoAnnualRewards extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        new BukkitRunnable() {
+            public void run() {
+                String playerName = event.getPlayer().getName();
+                Calendar now = Calendar.getInstance();
+                int currentYear = now.get(Calendar.YEAR);
+                int lastClaimedYear = getConfig().getInt("rewards." + playerName + ".year", -1);
+
+                if (currentYear != lastClaimedYear) {
+                    String rewardCommand = getConfig().getString("reward-command").replace("%player%", playerName);
+                    getServer().dispatchCommand(getServer().getConsoleSender(), rewardCommand);
+                    event.getPlayer().sendMessage("You have claimed your annual reward.");
+
+                    getConfig().set("rewards." + playerName + ".year", currentYear);
+                    saveConfig();
+                }
+            }
+        }.runTaskLater(this, 1200L);
+    }
+}
